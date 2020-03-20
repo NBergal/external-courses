350d71657f2b67f02ca223d48c0556274469c25b
NBergal <57491088+NBergal@users.noreply.github.com>
2020-03-20 22:42:02 +0300

    DELETE ALL

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,7 +0,0 @@
-function displayData(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
-    }
-    console.log(array.length + ' elements');
-}
-module.exports = displayData
