6408a424ef90d8707219ac2bff1dc92e2ef2732e
NBergal <57491088+NBergal@users.noreply.github.com>
2020-03-20 17:59:02 +0300

    Done homework 20_03

---
+++ src/ex6_js-array-methods/task-05.js
@@ -0,0 +1,8 @@
+function executeMapFunction(array, callback) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i, array));
+    }
+    return result;
+}
+module.exports = executeMapFunction
