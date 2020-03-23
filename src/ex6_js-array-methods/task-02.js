c178c83a74cebcd18e97553c8f9681dd233375af
NBergal <57491088+NBergal@users.noreply.github.com>
2020-03-23 19:07:07 +0300

    DELETE ALL

--- src/ex6_js-array-methods/task-02.js
+++
@@ -1,9 +0,0 @@
-function executeSomeFunction(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-        if (callback(array[i], i, array)) {
-            return true;
-        }
-    }
-    return false;
-}
-module.exports = executeSomeFunction
