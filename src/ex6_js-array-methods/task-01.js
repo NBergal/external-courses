22e517366c0bd4e82a35c94120fa4fbddb91fb5b
NBergal <57491088+NBergal@users.noreply.github.com>
2020-03-24 17:14:07 +0300

    Fixed homework 24_03

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,10 +1,12 @@
 function sliceFunction(array, begin = 0, end = array.length) {
     let result = [], beginValue = begin, endValue = end;
-    if (begin >= 0 && end < 0) {
-        endValue = array.length + end;
-    }
-    else if (begin < 0 && end < 0) {
+    if (begin < 0) {
         beginValue = array.length + begin;
+        if (end < 0) {
+            endValue = array.length + end;
+        }
+    }
+    else if (begin >= 0 && end < 0) {
         endValue = array.length + end;
     }
     for (let i = beginValue; i < endValue; i++) {
