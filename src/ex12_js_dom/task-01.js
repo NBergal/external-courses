060269932f74993ab5bb3fc934d9a95be0ddc139
NBergal <57491088+NBergal@users.noreply.github.com>
2020-04-12 00:22:54 +0300

    Fixed homework 12_04

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,29 +1,31 @@
-"use strict"
+'use strict'
 let imageArray = [
     './assets/images/photo-1551829142-d9b8cf2c9232.jpg',
     './assets/images/photo-1522748906645-95d8adfd52c7.jpg',
     './assets/images/photo-1525230071276-4a87f42f469e.jpg',
 ];
 let sliderImage = document.getElementById('image-current');
-let i = imageArray.length;
+let currentImageIndex = imageArray.length;
 window.onload = function() {
-    sliderImage.innerHTML = "<img class='image' src=" + imageArray[i - 1] + " alt='current image'>";
+    sliderImage.innerHTML = `<img class='image' src=${imageArray[currentImageIndex - 1]} alt='current image'>`;
 };
 function showNextImage() {
-    if (i < imageArray.length) {
-        i++;
+    if (currentImageIndex < imageArray.length) {
+        currentImageIndex++;
+    } else {
+        currentImageIndex = 1;
     }
-    else {
-        i = 1;
-    }
-    sliderImage.innerHTML = "<img class='image image_next' src=" + imageArray[i - 1] + " alt='current image'>";
+    sliderImage.innerHTML = `<img class='image image_next' src=${imageArray[currentImageIndex - 1]} alt='current image'>`;
 }
 function showPreviousImage() {
-    if (i < imageArray.length + 1 && i > 1) {
-        i--;
+    if (currentImageIndex < imageArray.length + 1 && currentImageIndex > 1) {
+        currentImageIndex--;
+    } else {
+        currentImageIndex = imageArray.length;
     }
-    else {
-        i = imageArray.length;
-    }
-    sliderImage.innerHTML = "<img class='image image_prev' src=" + imageArray[i - 1] + " alt='current image'>";
+    sliderImage.innerHTML = `<img class='image image_prev' src=${imageArray[currentImageIndex - 1]} alt='current image'>`;
 }
+document.getElementsByClassName('button_prev')[0].addEventListener('mousedown', showPreviousImage);
+document.getElementsByClassName('button_prev')[0].removeEventListener('mouseup', showPreviousImage);
+document.getElementsByClassName('button_next')[0].addEventListener('mousedown', showNextImage);
+document.getElementsByClassName('button_prev')[0].removeEventListener('mouseup', showNextImage);
